af38cef51b5a9b503f3d975ee0b35e56a642bb35
72003459 <72003459@gmail.com>
2024-09-09 18:31:33 -0500

    Refactorizacion del ejem02

--- Ejemploo2.py
+++ Ejemploo2.py
@@ -1,12 +1,12 @@
-def calcular(a, b, c):
-    res = a * b + c
-    return res
+def calcular(valormulti1, valormulti2, sumatoria):
+    respuesta = valormulti1 * valormulti2 + sumatoria
+    return respuesta
 
-def principal():
-    x = 5
-    y = 3
-    z = 7
-    resultado = calcular(x, y, z)
-    print("El resultado es:", resultado)
+if __name__ == "__main__":
+    valormulti1 = float(input('Valor de multiplicacion 1: '))
+    valormulti2 = float(input('Valor de multiplicacion 2: '))
+    sumatoria = float(input('valor de suma: '))
+    respuesta = calcular(valormulti1, valormulti2, sumatoria)
+    print(f"{valormulti1}*{valormulti2}+{sumatoria}={respuesta}")
 
-principal()
+
